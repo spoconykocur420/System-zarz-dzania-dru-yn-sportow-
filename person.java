b4ec80508da5dd5c7399c3e87cbc89b267074709
spoconykocur420 <bbartekak777@gmail.com>
2025-03-02 20:48:49 +0100

    pierwsza deklaracja

---
+++ person.java
@@ -0,0 +1,15 @@
+
+abstract class Person {
+    String firstName;
+    String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
---
+++ pozycja.java
@@ -0,0 +1,3 @@
+enum Position {
+    GOALKEEPER, DEFENDER, MIDFIELDER, FORWARD
+}
